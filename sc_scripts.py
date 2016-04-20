f28401f2d4ecd0d3154e39b4dc2392c3d5e31dd7
JaksaVucicevic <jaksa.vucicevic@gmail.com>
2016-04-20 14:49:23 +0200

    Added sc_scripts.py for import

---
+++ sc_scripts.py
@@ -0,0 +1,6 @@
+from calculations import *
+from formulae import *
+from schemes import *
+from data_types import *
+from impurity_solvers import *
+
--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 from calculations import *
 from formulae import *
 from schemes import *
+from data_types import *
+from impurity_solvers import *
 
 
 #quit()
